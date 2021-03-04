50ccc508a0aaa33173271218b90d4263d42111cf
YeiDiaz <yei.diaz93@gmail.com>
2021-03-04 01:59:56 +0000

    Landingpagefinal

---
+++ src/js/component/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Footer() {
+	return (
+		<div className="bg-dark text-center text-white">
+			<p className="m-0 p-5">Copyright &copy; Your Website 2021</p>
+		</div>
+	);
+}
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,51 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { Navbar } from "./navbar";
+import { Jumbotron } from "./jumbotron";
+import { Card } from "./cards";
+import { Footer } from "./footer";
+
+const cardArray = {
+	imageURL: "https://picsum.photos/seed/picsum/536/354",
+	cardTitle: "Card 1",
+	cardDescription:
+		"Lorem ipsum dolor sit amet consectetur adipiscing elit className ut, tempor ad interdum nisi cubilia sociosqu augue vivamus.",
+	buttonURL: "https://getlorem.com/es/",
+	buttonLabel: "Find Out More!"
+};
 
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<Navbar />
+			<div className="container">
+				<Jumbotron />
+				<div className="row">
+					<Card
+						imageURL={cardArray.imageURL}
+						cardTitle={cardArray.cardTitle}
+						cardDescription={cardArray.cardDescription}
+						buttonURL={cardArray.buttonURL}
+						buttonLabel={cardArray.buttonLabel}
+					/>
+					<Card
+						imageURL={cardArray.imageURL}
+						cardTitle={cardArray.cardTitle}
+						cardDescription={cardArray.cardDescription}
+						buttonURL={cardArray.buttonURL}
+						buttonLabel={cardArray.buttonLabel}
+					/>
+					<Card
+						imageURL={cardArray.imageURL}
+						cardTitle={cardArray.cardTitle}
+						cardDescription={cardArray.cardDescription}
+						buttonURL={cardArray.buttonURL}
+						buttonLabel={cardArray.buttonLabel}
+					/>
+				</div>
+			</div>
+			<Footer />
 		</div>
 	);
 }
